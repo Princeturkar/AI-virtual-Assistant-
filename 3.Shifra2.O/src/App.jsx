fd7779a463c6856a93b5c498e2dfa3baefba5ddc
Prince Turkar <168743619+Princeturkar@users.noreply.github.com>
2025-12-05 23:43:00 +0530

    Refactor App component for improved readability

--- 3.Shifra2.O/src/App.jsx
+++ 3.Shifra2.O/src/App.jsx
@@ -1,38 +1,51 @@
-import React, { useContext } from 'react'
-import "./App.css"
-import va from "./assets/ai.png"
+import React, { useContext } from "react";
+import "./App.css";
+import va from "./assets/ai.png";
 import { CiMicrophoneOn } from "react-icons/ci";
-import { datacontext } from './context/UserContext';
-import speakimg from "./assets/speak.gif"
-import aigif from "./assets/aiVoice.gif"
+import { datacontext } from "./context/UserContext";
+import speakimg from "./assets/speak.gif";
+import aigif from "./assets/aiVoice.gif";
+
 function App() {
-  let {recognition,speaking,setSpeaking,prompt,response,setPrompt,setResponse}=useContext(datacontext)
+  const {
+    recognition,
+    speaking,
+    setSpeaking,
+    prompt,
+    response,
+    setPrompt,
+    setResponse,
+  } = useContext(datacontext);
 
-  
   return (
-    <div className='main'>
-      <img src={va} alt="" id="shifra"/>
-      <span>I'm shifra,Your Advanced Virtual Assistant</span>
-      {!speaking?
-      <button onClick={()=>{ 
-    setPrompt("listening...")
-    setSpeaking(true)
-    setResponse(false)
-  recognition.start()
-      }}>Click here <CiMicrophoneOn /></button>
-    :
-    <div className='response'>
-      {!response?
-      <img src={speakimg} alt="" id="speak" />
-      :
-      <img src={aigif} alt="" id="aigif" />}
-<p>{prompt}</p>
-    </div>
+    <div className="main">
+      <img src={va} alt="" id="shifra" />
+      <span>I'm Shifra, Your Advanced Virtual Assistant</span>
 
-      }
-     
+      {!speaking ? (
+        <button
+          onClick={() => {
+            setPrompt("Listening...");
+            setSpeaking(true);
+            setResponse(false);
+            recognition.start();
+          }}
+        >
+          Click here <CiMicrophoneOn />
+        </button>
+      ) : (
+        <div className="response">
+          {!response ? (
+            <img src={speakimg} alt="speaking" id="speak" />
+          ) : (
+            <img src={aigif} alt="ai speaking" id="aigif" />
+          )}
+          <p>{prompt}</p>
+        </div>
+      )}
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
+
